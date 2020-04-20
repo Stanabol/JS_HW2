6877ad47887d0f3f63405d40618881b8cdf59ecc
Stanabol <Aspectdevice1990@gmail.com>
2020-04-20 20:39:24 +0300

    script06

--- script06.js
+++ script06.js
@@ -1,10 +1,27 @@
 // Написать конвертор валют. Пользователь вводит количество USD, выбирает, в какую валюту хочет перевести: EUR, UAN или AZN, и получает в ответ соответствующую сумму.
 let usd = +prompt("Введите USD");
+let EUR = 0.92;
+let UAH = 27.08;
+let AZN = 1.70;
 let convertEUR = confirm("Перевести в - EUR?");
-let convertUAH = confirm("Перевести в - UAH?");
-let convertAZN = confirm("Перевести в - AZN?");
-let EUR = 0;
-let UAH = 0;
-let AZN = 0;
+if (convertEUR == true) {
+    let rezultEUR = usd * EUR;
+    alert(`Из ${usd}$ вы получите ${rezultEUR}€`);
+} else {
+    let convertAZN = confirm("Перевести в - AZN?");
+    if (convertAZN == true) {
+        let rezultAZN = usd * AZN;
+        alert(`Из ${usd}$ вы получите ${rezultAZN}₼`);
+    } else {
+        let convertUAH = confirm("Перевести в - UAH?");
+        if (convertUAH == true) {
+            let rezultUAH = usd * UAH;
+            alert(`Из ${usd}$ вы получите ${rezultUAH}₴`);
+        }
+    }
+}
+
+
+
 
 
