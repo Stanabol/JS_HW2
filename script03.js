c7ef7d4f7dd2b7c8100d4db3fc8c4b55e8f0c6ab
Stanabol <Aspectdevice1990@gmail.com>
2020-04-20 19:45:54 +0300

    script03

--- script03.js
+++ script03.js
@@ -1,2 +1,15 @@
 // Запросить у пользователя трехзначное и число и проверить, есть ли в нем одинаковые цифры.
-let
+let num = +prompt("Введите трехзначное число");
+if (num <= 999 && num >= 100) {
+    let lastnum = num % 10;
+    let secondnum = ((num % 100) - lastnum) / 10;
+    let firstnum = (num - (num % 100)) / 100;
+    if (firstnum == secondnum || firstnum == lastnum || secondnum == lastnum) {
+        alert("В данном числе есть одинаковые цифры");
+    } else {
+        alert("В данном числе нет одинаковых цифр");
+    }
+} else {
+    alert("Вы ввели не трехзначное число");
+
+}
